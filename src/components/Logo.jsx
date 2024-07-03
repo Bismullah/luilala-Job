77184362d6d84e846e9a850f291d05844fb11d6c
Bismiallah <bismullahwafadar5@gmail.com>
2024-07-03 06:06:12 -0700

    edit image urle

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-
+import logo from "../src/assets/logo.png";
 export default function Logo() {
   return (
     <div className="company-logo flex items-center justify-center">
       <Link to="/" className="block">
-        <img src="../src/assets/logo.png" alt="Company Logo" className="w-16 scale-1 h-auto" />
+        <img src={logo} alt="Company Logo" className="w-16 scale-1 h-auto" />
       </Link>
     </div>
   );
